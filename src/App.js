a3336e3570c7b30451343ec6d276437543117bfc
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-04-27 17:43:35 +0200

    fix: Add key to FormSplitBill. This will fix the issue of having the same states when switching between friends

--- src/App.js
+++ src/App.js
@@ -97,6 +97,7 @@ export default function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
